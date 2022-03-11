b2db3613dcbe6bf52f39c96b78d8225f70998739
Polina <p.pupsic2.0.2@gmail.com>
2022-03-11 17:53:24 +0300

    changed controller to rest-controller and response in json

--- src/main/java/com/web/kpp/controller/TriangleIdentificationController.java
+++ src/main/java/com/web/kpp/controller/TriangleIdentificationController.java
@@ -1,35 +1,28 @@
 package com.web.kpp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 import com.web.kpp.entity.Triangle;
 import com.web.kpp.service.TriangleIdentificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TriangleIdentificationController {
     @Autowired
     private TriangleIdentificationService triangleIdentificationService;
     @GetMapping("/identification")
-    public String triangleParams(
+    public ResponseEntity<Object> triangleParams(
             @RequestParam(name="side1", defaultValue = "0") int side1,
             @RequestParam(name="side2", defaultValue = "0") int side2,
-            @RequestParam(name="side3", defaultValue = "0") int side3,
-            Model model
+            @RequestParam(name="side3", defaultValue = "0") int side3
     ) {
         Triangle triangle = triangleIdentificationService.getTriangle(side1, side2, side3);
 
-        model.addAttribute("side1", side1);
-        model.addAttribute("side2", side2);
-        model.addAttribute("side3", side3);
-
-        model.addAttribute("rectangular", triangle.isRectangular ? "да" : "нет");
-        model.addAttribute("isosceles", triangle.isIsosceles ? "да" : "нет");
-        model.addAttribute("equilateral", triangle.isEquilateral ? "да" : "нет");
-
-        return "home";
+        return new ResponseEntity<>(triangle, HttpStatus.OK);
     }
 
 }
