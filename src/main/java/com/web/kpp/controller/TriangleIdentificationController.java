f352d41ebc5387ae453127eddc68aa2b65f635ce
Polina <p.pupsic2.0.2@gmail.com>
2022-03-27 20:58:44 +0300

    completed second lab

--- src/main/java/com/web/kpp/controller/TriangleIdentificationController.java
+++ src/main/java/com/web/kpp/controller/TriangleIdentificationController.java
@@ -1,25 +1,38 @@
 package com.web.kpp.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 
 import com.web.kpp.service.TriangleIdentificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.constraints.Positive;
 
 @RestController
+@Validated
 public class TriangleIdentificationController {
     @Autowired
     private TriangleIdentificationService triangleIdentificationService;
+
+    private static final Logger logger = LogManager.getLogger(ResponseEntityExceptionHandler.class);
+
     @GetMapping("/identification")
     public ResponseEntity<Object> triangleParams(
-            @RequestParam(name="side1", defaultValue = "0") int side1,
-            @RequestParam(name="side2", defaultValue = "0") int side2,
-            @RequestParam(name="side3", defaultValue = "0") int side3
+            @RequestParam(name="side1", required = true) @Positive(message = "Value must be positive!") int side1,
+            @RequestParam(name="side2", required = true) @Positive(message = "Value must be positive!") int side2,
+            @RequestParam(name="side3", required = true) @Positive(message = "Value must be positive!") int side3
     ) {
-        return new ResponseEntity<>(triangleIdentificationService.identificate(side1, side2, side3), HttpStatus.OK);
+        logger.info("GET /identification");
+
+        return ResponseEntity.ok(
+                triangleIdentificationService.identify(side1, side2, side3)
+        );
     }
 
 }
--- src/main/java/com/web/kpp/service/TriangleIdentificationService.java
+++ src/main/java/com/web/kpp/service/TriangleIdentificationService.java
@@ -1,13 +1,29 @@
 package com.web.kpp.service;
 
+import com.web.kpp.exceptions.TriangleDoesNotExistException;
 import org.springframework.stereotype.Service;
 import com.web.kpp.entity.TriangleIdentification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Service
 public class TriangleIdentificationService {
-    public TriangleIdentification identificate(int side1, int side2, int side3) {
+    private static final Logger logger = LogManager.getLogger(TriangleIdentificationService.class);
+    private void validateTriangle (int side1, int side2, int side3) {
+        if (
+                side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+        ) {
+            logger.error("Identify error! Wrong sides length.");
+            throw new TriangleDoesNotExistException("Triangle does not exist!");
+        }
+    }
+
+    public TriangleIdentification identify(int side1, int side2, int side3) {
+        validateTriangle(side1, side2, side3);
+
         TriangleIdentification triangleIdentification = new TriangleIdentification();
+
         triangleIdentification.setIsEquilateral(side1 == side2 && side2 == side3);
         triangleIdentification.setIsIsosceles(side1 == side2 || side2 == side3 || side1 == side3);
         triangleIdentification.setIsRectangular(
@@ -15,6 +31,8 @@ public class TriangleIdentificationService {
                         Math.pow(side2, 2) == Math.pow(side1, 2) + Math.pow(side3, 2) ||
                         Math.pow(side3, 2) == Math.pow(side1, 2) + Math.pow(side2, 2)
         );
+
+        logger.info("Successful identify!");
 
         return triangleIdentification;
     }
